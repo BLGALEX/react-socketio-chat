b4c369857a3f780b4c2736e2b5056baf4d14eeb8
BLGALEX <lepehaleha@bk.ru>
2022-05-18 11:25:29 +0900

    init commit2

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# react-socketio-chat
+# TypeScript / React / NodeJS / SocketIO chat sample application
+
+
+## Requirements
+
+NodeJS / npm
+TypeScript 2.9.2
+
+## Run
+
+Build and run NodeJS server first:
+
+    $ cd server
+    $ npm install
+    $ npx tsc
+    $ node dist/index.js
+
+Client:
+
+    $ cd client
+    $ npm install
+    $ npm start
+
---
+++ server/src/SharedTypes.ts
@@ -0,0 +1,44 @@
+export function validNick(nick: string): boolean {
+  return nick.length >= 3 && /^[a-zA-Z0-9_-]+$/.test(nick);
+}
+
+export interface User {
+  guest: boolean;
+  token: string;
+  n: number;
+  nick: string | null;
+}
+
+export interface BasicRoom {
+  name: string;
+  id: number;
+}
+
+export type ChatEvent = NameChangeEvent | MessageEvent | MeEvent;
+
+export interface NameChangeEvent {
+  type: 'NameChange';
+  n: number;
+  guest: boolean;
+  oldNick: string;
+  newNick: string;
+  date: number;
+}
+
+export interface MessageEvent {
+  type: 'Message';
+  n: number;
+  guest: boolean;
+  nick: string;
+  text: string;
+  date: number;
+}
+
+export interface MeEvent {
+  type: 'Me';
+  n: number;
+  guest: boolean;
+  nick: string;
+  text: string;
+  date: number;
+}
